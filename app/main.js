9d5d432a1165f1a2ad6ba54bef6e08037cd88325
Mechatron <soni.anurag0761@gmail.com>
2024-07-16 00:10:15 +0530

    Stage 2

--- app/main.js
+++ app/main.js
@@ -5,9 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-   connection.on('connection',()=>{
-    console.log("+PONG\r\n");
-   })
+   connection.write('+PONG\r\n');
 });
 //
 server.listen(6379, "127.0.0.1");
