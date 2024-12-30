d1b0a85890736fef848d4053fa9453c5ec40590e
Mechatron <soni.anurag0761@gmail.com>
2024-12-31 00:11:29 +0530

    Remove ignored files and update .gitignore

--- commandServer/pipeClient.ps1
+++
@@ -1,7 +0,0 @@
-$pipeClient = New-Object System.IO.Pipes.NamedPipeClientStream(".", "mypipe", [System.IO.Pipes.PipeDirection]::Out)
-$pipeClient.Connect()
-$writer = New-Object System.IO.StreamWriter($pipeClient)
-$writer.AutoFlush = $true
-$writer.WriteLine("PING")
-$writer.Close()
-$pipeClient.Close()
